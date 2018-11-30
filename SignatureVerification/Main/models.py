489f03a6c9b709895a2938c64a455959ea0c6edb
Daksh Pokar <dakshpokar@gmail.com>
2018-11-30 19:43:34 +0530

    Added Customer Database

--- SignatureVerification/Main/models.py
+++ SignatureVerification/Main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class CustomerDetails(models.Model):
+    c_id = models.CharField(max_length=250)
+    c_name = models.CharField(max_length=250)
+
