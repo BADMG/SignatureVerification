4ad5d5850eedde1ee9413bf02964e207ebded60a
Saurabh Bora <saurabhbora@gmail.com>
2018-12-01 14:14:16 +0530

    Seperated Attachment and CustomerDetails Model

--- SignatureVerification/Main/models.py
+++ SignatureVerification/Main/models.py
@@ -4,15 +4,19 @@ from django.db import models
 
 
 def user_directory(instance, filename):
-    return str(instance.c_id) + "/" + filename
+    return str(instance.customerdetails.c_id) + "/" + filename
 
 
 class CustomerDetails(models.Model):
     c_id = models.CharField(max_length=250)
     c_name = models.CharField(max_length=250)
-    image = models.FileField(upload_to=user_directory)
-
-
+    
+    
 class VerificationDetails(models.Model):
     c_id = models.CharField(max_length=250)
     image = models.FileField(upload_to=user_directory)
+
+    
+class Attachment(models.Model):
+    customerdetails = models.ForeignKey(CustomerDetails,on_delete=models.CASCADE)
+    file = models.FileField(upload_to=user_directory)
