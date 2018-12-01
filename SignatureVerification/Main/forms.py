ecaf820c13823e0948b3621a2457ee0d5bc795e8
Daksh Pokar <dakshpokar@gmail.com>
2018-12-01 14:49:21 +0530

    Old

--- SignatureVerification/Main/forms.py
+++ SignatureVerification/Main/forms.py
@@ -1,23 +1,15 @@
 from django import forms
-from multiupload.fields import MultiFileField
-from Main.models import CustomerDetails, VerificationDetails, Attachment
+from Main.models import CustomerDetails, VerificationDetails
 
 
 class CustomerForm(forms.ModelForm):
     c_id = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Customer ID', 'class':'form-control mb-4'}))
     c_name = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Customer Name', 'class':'form-control mb-4'}))
+    image = forms.FileField(label="")
 
     class Meta:
         model = CustomerDetails
-        fields = ('c_name', 'c_id')
-    
-    files = MultiFileField(min_num=1, max_num=5, max_file_size=1024*1024*5)
-    
-    def save(self, commit=True):
-        instance = super(CustomerForm, self).save(commit)
-        for each in self.cleaned_data['files']:
-            Attachment.objects.create(file=each, customerdetails=instance)
-        return instance
+        fields = ('c_name', 'c_id', 'image', )
 
 
 class VerificationForm(forms.ModelForm):
@@ -27,4 +19,3 @@ class VerificationForm(forms.ModelForm):
     class Meta:
         model = VerificationDetails
         fields = ('c_id', 'image')
-
