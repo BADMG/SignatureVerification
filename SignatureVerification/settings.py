b3027af578fc1afaacea31f8bc9b430fbe26faaf
Daksh Pokar <dakshpokar@gmail.com>
2018-12-02 15:07:32 +0530

    Heroku2

--- SignatureVerification/settings.py
+++ SignatureVerification/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_vxe1@q(n^(9i42n4m&pnd=6rh_o!y25p5a!!mqklks$8xi()8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = FALSE
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', ]
 
