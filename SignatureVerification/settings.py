a48082e497070f39ccb5c5b1e39400515c90867e
Daksh Pokar <dakshpokar@gmail.com>
2018-12-02 16:00:59 +0530

    'heroku6'

--- SignatureVerification/settings.py
+++ SignatureVerification/settings.py
@@ -61,6 +61,7 @@ PROJECT_PATH = os.path.abspath((os.path.dirname(__name__)))
 STATICFILES_DIRS = [
     os.path.join(PROJECT_PATH, 'static')
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
