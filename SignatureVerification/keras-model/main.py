31913d8b6b05c56ff38a04ffed24d09b8198c53d
Daksh Pokar <dakshpokar@gmail.com>
2018-12-15 14:23:16 +0530

    removed unnecessary

--- SignatureVerification/keras-model/main.py
+++ SignatureVerification/keras-model/main.py
@@ -9,7 +9,7 @@ from keras.regularizers import l2
 
 img_width = 300
 img_height = 150
-batch = 296
+batch = 128
 
 def modelCreator():
     modelA = models.Sequential()
@@ -55,7 +55,7 @@ model.summary()
 
 path_to_dataset = os.getcwd() + "\\dataset"
 #For training
-for x in range(1, 6):
+for x in range(1, 5):
     if(x != 4):
         forge = []
         forge.extend(os.listdir(path_to_dataset+str(x)+ "\\forge"))
@@ -118,11 +118,11 @@ for x in range(1, 6):
         real_images = real_images.reshape(real_images.shape[0], img_width, img_height, 1)
         forged_images = forged_images.reshape(forged_images.shape[0], img_width, img_height, 1)
         anchor_images = anchor_images.reshape(anchor_images.shape[0], img_width, img_height, 1)
-        model.fit([anchor_images,real_images,forged_images], y, epochs = 100, batch_size = 30)
+        model.fit([anchor_images,real_images,forged_images], y, epochs = 30, batch_size = 15)
 
 
 #For Testing
-for x in range(6, 7):
+for x in range(4, 5):
     
     forge = []
     forge.extend(os.listdir(path_to_dataset+str(x)+ "\\forge"))
@@ -198,8 +198,8 @@ for x in range(6, 7):
 t = 3
 x = 0
 l = []
-k1 = 288
-k2 = 24
+k1 = 90
+k2 = 5
 values = []
 
 for t in np.arange(1, 3, 1):
@@ -252,7 +252,7 @@ for t in np.arange(1, 3, 1):
 
 '''
 values = []
-for t in np.arange(1, 70, 1):
+for t in np.arange(1, 800, 1):
     x = 0
     for i in l:
         if(i>t):
